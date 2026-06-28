9b244083d62f09405a04414fac7890f8f43b157b
buildwithvishal <vishallakshya2004@gmail.com>
2026-06-28 18:43:16 +0530

    ATS score feature completed

--- server/services/geminiService.js
+++ server/services/geminiService.js
@@ -1,8 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 
 export const analyzeResumeWithGemini = async (resumeText) => {
-  console.log("Gemini Key =", process.env.GEMINI_API_KEY);
-
   const ai = new GoogleGenAI({
     apiKey: process.env.GEMINI_API_KEY,
   });
@@ -10,9 +8,36 @@ export const analyzeResumeWithGemini = async (resumeText) => {
   const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
     contents: `
-You are an expert career coach.
+You are a professional ATS Resume Analyzer and Career Coach.
 
-Analyze this resume:
+Analyze the following resume and provide the output in EXACTLY this format:
+
+ATS SCORE: <score out of 100>
+
+STRENGTHS:
+- Point 1
+- Point 2
+- Point 3
+
+WEAKNESSES:
+- Point 1
+- Point 2
+- Point 3
+
+IMPROVEMENT SUGGESTIONS:
+- Point 1
+- Point 2
+- Point 3
+
+MISSING KEYWORDS:
+- Keyword 1
+- Keyword 2
+- Keyword 3
+
+FINAL VERDICT:
+<Short overall evaluation>
+
+Resume:
 
 ${resumeText}
 `,
